d81e1a662a86b5c47405a870d7b82db64231d884
Manikya <sabharwal.manikya@gmail.com>
2020-02-19 15:05:01 +0530

    Additional information on forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,15 @@
 const request = require("request");
 
 const forecast = ({latitude, longitude}, callback) => {
-    const url = 'https://api.darksky.net/forecast/512041ddde71d3f8cf23f3f3f8021b6a/' + longitude + ',' + latitude ;
+    console.log(latitude, longitude);
+    const url = 'https://api.darksky.net/forecast/512041ddde71d3f8cf23f3f3f8021b6a/' + latitude + ',' + longitude + '?units=si' ;
     request({url, json: true}, (error, { body }) => {
         if(error) {
             callback('Unable to connect to weather service!', undefined);
         } else if(body.error) {
             callback('Unable to find location.', undefined);
         } else {
-            callback(undefined, `${ body.hourly.summary } It is currently ${ body.currently.temperature} outside. Chances of rain are: ${body.currently.precipProbability * 100} %`);
+            callback(undefined, `${ body.hourly.summary } ${ body.daily.summary } It is currently ${ body.currently.temperature} outside. Chances of rain are: ${body.currently.precipProbability * 100} %`);
         }
     })
 };
